56dea47074b5dbc683db7728ee720d311aa8d3fb
Daniel Edvinsson <edvinsson.d@gmail.com>
2017-05-09 11:40:13 +0200

    Skapat utpasseringscontroller med tillhörande views. Bytt ut text "närvaro" till "inpassering" för att vara konsekvent med startsidan.

---
+++ Inpasseringssystem/Controllers/UtpasseringController.cs
@@ -0,0 +1,24 @@
+﻿using Inpasseringssystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inpasseringssystem.Controllers
+{
+    public class UtpasseringController : Controller
+    {
+        // GET: Utpassering
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Complete(Guest model)
+        {
+            ViewBag.Name = model.Name;
+            return View();
+        }
+    }
+}
