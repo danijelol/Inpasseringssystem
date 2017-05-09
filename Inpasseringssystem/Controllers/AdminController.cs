18dba2e8976d1b8e6de3b5ceffc8f20f74dda3e9
Daniel Edvinsson <edvinsson.d@gmail.com>
2017-05-09 16:25:41 +0200

    Påbörjat admindelen av prototypen. Skapat controller och vy med kontroll av lösenord.

---
+++ Inpasseringssystem/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Inpasseringssystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inpasseringssystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminStart(Guest model)
+        {
+            if (model.Password != "password")
+            {
+                ViewBag.Error = "Felaktigt lösenord!";
+                return View("Index");
+            }
+            return View();
+        }
+    }
+}
--- Inpasseringssystem/Models/guest.cs
+++ Inpasseringssystem/Models/guest.cs
@@ -11,5 +11,6 @@ namespace Inpasseringssystem.Models
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Visiting { get; set; }
+        public string Password { get; set; }
     }
 }
