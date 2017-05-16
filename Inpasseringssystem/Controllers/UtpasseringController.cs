9b565b13de386d49ead4ec891df6e4e881cae9c9
Daniel Edvinsson <edvinsson.d@gmail.com>
2017-05-16 09:50:56 +0200

    Lagt till felkontroller för förnamn och efternamn vid registrering av utpassering.

--- Inpasseringssystem/Controllers/UtpasseringController.cs
+++ Inpasseringssystem/Controllers/UtpasseringController.cs
@@ -17,6 +17,23 @@ namespace Inpasseringssystem.Controllers
 
         public ActionResult Complete(Guest model)
         {
+            if (model.Name == null && model.Surname == null)
+            {
+                ViewBag.ErrorName = "Du måste ange ditt förnamn";
+                ViewBag.ErrorSurname = "Du måste ange ditt efternamn";
+                return View("Index");
+            }
+            else if (model.Name == null)
+            {
+                ViewBag.ErrorName = "Du måste ange ditt förnamn";
+                return View("Index");
+            }
+            else if (model.Surname == null)
+            {
+                ViewBag.ErrorSurname = "Du måste ange ditt efternamn";
+                return View("Index");
+            }
+
             ViewBag.Name = model.Name;
             return View();
         }
